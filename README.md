4c64722e2b9d84452861281e57e4bbd4f78f0ad8
areeswp <bressanjulio@gmail.com>
2022-02-21 11:29:51 -0300

    bugfix; inclusão nova função; alterações no código (leitura final em dataframe e menos repetições no código).

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Código para coleta do preço de compra e venda do dólar, a partir do site ofic
 Possui duas funções: 
   1. Retorna os valores do dólar (compra e venda) para a data específica recebida pelo usuário, ou o primeiro dia útil anterior a data.
   2. Retorna os valores do dólar (compra e venda) para o último dia útil da primeira quinzena do mês anterior à data informada.
+  3. Retorna os valores do dólar (compra e venda) dentro de um período especificado por data inicial e data final.
 
 Nota-se que o programa possibilita a coleta de mais de um preço, porém não é recomendado fazer uma lista muito grande, para evitar problemas não recorrentes no site que possam crashar o código e resultar na perda dos valores antes da impressão final.
 
@@ -11,11 +12,11 @@ Também é recomendado realizar apenas as ações no terminal, e não interagir 
 
 [Link para o código](dolarScraper.py)
 ## Pré Requisitos
-É necessário instalar duas libs do python para o devido funcionamento do programa, essas são:
-  1. selenium
-     - (Utilizado para o funcionamento da raspagem dos dados)
-  2. webdriver-manager
-     - (Auxílio ao selenium)
+É necessário instalar as seguintes bibliotecas do python para o devido funcionamento do programa, essas são:
+  - **selenium**
+  - **webdriver-manager**
+  - **pandas**
+  - talvez seja necessária a **openpyxl** também
      
 Para ajuda nesse sentido, recomendo esse [link](https://computadorcomwindows.com/2018/01/19/tutorial-como-instalar-uma-biblioteca-python-no-computador/).
 Se o usuário ainda não possuir o python, sugiro esse [vídeo](https://www.youtube.com/watch?v=-RuY-rM-B4M).
@@ -25,8 +26,9 @@ Após a execução do código:
   2. Próximo input é a data desejada pelo usuário
   3. O retorno já é apresentado no terminal, porém também é armazenado em dois dicionários, um para dólar compra e outro para dólar venda
   4. Escolha do usuário:
-     - y leva o usuário a mais um input de data
-     - n encerra o programa, apresentando os dicionários com os dados armazenados.
+     - pedir mais datas
+     - Digitar "0" para encerrar o programa.
+  5. Após o usuário digitar "0", se apresenta os resultados em dataframes do pandas (baseado nos dicionários preenchidos), para facilitar a visualização dos resultados.
 
 Novamente aqui se recomenda não fazer uma lista muito grande, para evitar possíveis erros no site que resultem na perda dos dados armazenados.
 # DollarScraper EN-US
@@ -35,26 +37,28 @@ Code to get BRL (Brazilian Real) dollar exchange rate, from official Banco Centr
 It has two functions:
   1. Returns dollar exchange rate to the specific date received from the user, or the first business day before it.
   2. Returns dollar exchange rate to the last business day in the first 15-days (fortnight) of the month before the specified date.
+  3. Returns dollar exchange rate for a time period specified by initial and final dates.
 
 Please note that the program allows multiple price inputs, but it is not recommended to make a big list, in order to avoid ocasional problems in the site that might crash the code and result in the loss of values before final print.
 
 [Link for the code](dolarScraper.py)
 ## Precondition
-It is necessary to install two libraries for Python for proper functioning of the program, those are:
-  1. selenium
-     - (Used for web scraping)
-  2. wedriver-manager
-     - (Assistant to selenium)
+It is necessary to install the following libraries for Python for proper functioning of the program, those are:
+  - **selenium**
+  - **wedriver-manager**
+  - **pandas**
+  - **and perhaps you may need openpyxl as well**
 
 If you need help in this sense, I recommend this [link](https://packaging.python.org/en/latest/tutorials/installing-packages/).
 ## Use
 After executing the code:
-  1. First user has to input option: 1 for exact date and 2 for date from the month before
+  1. First user has to input option: 1 for exact date, 2 for date from the month before or 3 for search by time period
   2. The next input is the user's date
   3. The return is already presented in the terminal, but it is also stored in two dictionaries.
   4. User choice:
-     - y takes the user to a new date input
-     - n kills the program and prints the dictionaries with their stored data.
+     - ask for a new date
+     - type "0" to end the program
+  5. After the user types "0", it is printed the dataframes from pandas (based on the stored dictionaries) for easier visualization of the results.
 
 Once again, it it recommended not to store too many dates, to avoid non-recurring errors that may result in the loss of stored data.
 
--- dolarScraper.py
+++ dolarScraper.py
@@ -1,5 +1,5 @@
-# import lib
-# pip selenium, webdriver-manager
+# import libs
+# pip selenium, webdriver-manager, pandas
 
 import datetime
 from datetime import timedelta
@@ -9,86 +9,128 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import sys
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 
 # configurando selenium
 # configuring selenium
 driverOptions = webdriver.ChromeOptions()
 driverOptions.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=driverOptions)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=driverOptions)
 driver.get(
     "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/aplicacao#!/recursos/CotacaoDolarDia#eyJmb3JtdWxhcmlvIjp7IiRmb3JtYXQiOiJqc29uIiwiJHRvcCI6MTAwfX0="
 )
 driver.maximize_window()
 time.sleep(1)
-# id do campo de data de cotação do site
-# website's quotation date id field
-element = driver.find_element(By.ID, "param0")
-
+try: # busca o id do campo da data de cotação do site; tries to locate the website's quotation date id field
+    element = driver.find_element(By.ID, "param0")
+except: # mensagem de erro e finalização do programa caso não encontre o elemento necessário; error message and program end in case it doesn't find necessary element
+    sys.exit(
+        "Impossível de alcançar o site e localizar o elemento necessário; favor tente novamente mais tarde ou confira se houve alteração no HTML do site."
+    )
 # declaração das variáveis globais
 # global variables
-aimDate = datetime.date(2022, 1, 20)
-userDate = datetime.date(2022, 1, 20)
-# variável de 1 dia para alterar a variável data
-# 1 day variable to change the date variable
-day = timedelta(days=1)
-# lista com os dias do final de semana
-# list with weekend days
-weekend = ["Saturday", "Sunday"]
-# criação da variável aim, com a data fixada no dia 15 (ano e mês irrelevantes)
-# variable aim, with date fixed in day 15 (year and month irrelevant)
-aim = datetime.date(2021, 12, 15)
+aimDate = datetime.date(2022, 1, 20) # data que o usuário busca, convertida em formato datetype; the date that the user aims, converted in datetype format
+userDate = datetime.date(2022, 1, 20) # input do usuário, formato inicial em string e depois convertida para datetype; user input, string initial format, later converted to datetype
+finalDate = datetime.date(2022, 1, 20) # em busca por período armazena a data final; in search by period it stores the final date
+day = timedelta(days=1) # variável de 1 dia; 1 day variable
+weekend = ["Saturday", "Sunday"] # lista com os dias do final de semana; list with weekend days
 dolarCompra = "0"
 dolarVenda = "0"
 dataCompra = {}
 dataVenda = {}
-dataExata = "0"
+userOption = "0"
 
 print(
     "Essas são as minhas funções: \n" 
     "(1) = Valores do dólar para a data especificada (ou primeiro dia útil anterior à data)\n"
     "(2) = Valores do dólar no último dia útil na primeira quinzena do mês anterior à data especificada\n"
+    "(3) = Valores do dólar para o período especificado (entre duas datas)\n"
     )
-dataExata = input("Por favor, selecione a função desejada: ")
-while dataExata not in ("1","2"):
-    print("Por favor, digite uma opção válida: (1 ou 2) ")
-    dataExata = input("Por favor, selecione a função desejada: ")
-# input da data pelo usuário
-# user's date input
+userOption = input("Por favor, selecione a função desejada: ")
+while userOption not in ("1","2","3"):
+    print("Por favor, digite uma opção válida: (1, 2 ou 3) ")
+    userOption = input("Por favor, selecione a função desejada: ")
+
+# função para encerrar o programa
+# exit program function
+def endProgram():
+    print(pd.DataFrame(list(dataCompra.items()),
+                    columns=['Dia', 'Preço de Compra'])
+    )
+    print(pd.DataFrame(list(dataVenda.items()),
+                    columns=['Dia', 'Preço de Venda'])
+    )
+    driver.close()
+    sys.exit("\nFim do programa")
+
+# função de input da data pelo usuário
+# user's date input function
 def inputDate():
     isValid = False
     global aimDate
     global userDate
+    global finalDate
     while not isValid:
-        userDate = input(
-            "Digite a data da movimentação (dd/mm/yyyy), ou 0 para sair do programa: "
-        )
-        time.sleep(1)
-        if userDate == "0":
-            print(
-                "Lista das datas e respectivos preços de compra do dólar consultados:\n"
-                + str(dataCompra)
+        if userOption in ("1","2"):
+            userDate = input(
+                "Digite a data da movimentação (dd/mm/yyyy), ou 0 para sair do programa: "
             )
-            print(
-                "\nLista das datas e respectivos preços de venda do dólar consultados: \n"
-                + str(dataVenda)
+            time.sleep(1)
+            if userDate == "0": # confere o input para encerrar o programa; checks for the input to break the program
+                endProgram()
+            try:  # conferência no formato de dd/mm/yyyy e armazenamento nas variáveis; checking format dd/mm/yyyy and store in variables
+                aimDate = datetime.datetime.strptime(userDate, "%d/%m/%Y")
+                # altera variável userDate em formato de data
+                # changes userDate variable into a date format
+                userDate = datetime.datetime.strptime(userDate, "%d/%m/%Y")
+                isValid = True
+            except:  # se o input tiver formato diferente, tente novamente; if different format, try again
+                print("Verifique a data e tente novamente!")
+        if userOption == "3": # verifica a função de busca por período e pede pela data inicial e final; verifies the function and asks for initial and final dates
+            userDate = input(
+                "Digite a data inicial do período desejado (dd/mm/yyyy), ou 0 para sair do programa: "
             )
-            driver.close()
-            sys.exit("\nFim do programa")
-        try:  # conferência no formato de dd/mm/yyyy e armazenamento nas variáveis; checking format dd/mm/yyyy and store in variables
-            aimDate = datetime.datetime.strptime(userDate, "%d/%m/%Y")
-            # altera variável userDate em formato de data
-            # changes userDate variable into a date format
-            userDate = datetime.datetime.strptime(userDate, "%d/%m/%Y")
-            isValid = True
-        except:  # se o input tiver formato diferente, tente novamente; if different format, try again
-            print("Verifique a data e tente novamente!")
-
-
+            if userDate == "0": # confere o input para encerrar o programa; checks for the input to break the program
+                endProgram()
+            finalDate = input(
+                "Digite a data final do período desejado (dd/mm/yyyy), ou 0 para sair do programa: "
+            )
+            if finalDate == "0" : # confere o input para encerrar o programa; checks for the input to break the program
+                endProgram()
+            time.sleep(1)
+            while userDate == finalDate: # evita erros por datas iguais; avoids errors by having the same date
+                print ("Favor digitar datas diferentes ou utilizar uma das outras funções.")
+                userDate = input(
+                    "Digite a data inicial do período desejado (dd/mm/yyyy), ou 0 para sair do programa: "
+                )
+                if userDate == "0": # confere o input para encerrar o programa; checks for the input to break the program
+                    endProgram()
+                finalDate = input(
+                "Digite a data final do período desejado (dd/mm/yyyy), ou 0 para sair do programa: "
+                )
+                if finalDate == "0" : # confere o input para encerrar o programa; checks for the input to break the program
+                    endProgram()
+            try:  # conferência no formato de dd/mm/yyyy e armazenamento nas variáveis; checking format dd/mm/yyyy and store in variables
+                aimDate = datetime.datetime.strptime(userDate, "%d/%m/%Y")
+                finalDate = datetime.datetime.strptime(finalDate, "%d/%m/%Y")
+                userDate = datetime.datetime.strptime(userDate, "%d/%m/%Y")
+                if finalDate < aimDate: # se data inicial for maior que a final inverte os valores; changes the values if initial date comes aftes final date
+                    temp = finalDate
+                    finalDate = aimDate
+                    aimDate = temp
+                    isValid = True
+                else:
+                    isValid = True
+            except:  # se o input tiver formato diferente, tente novamente; if different format, try again
+                print("Verifique a data e tente novamente!")
 inputDate()
 
 # função com while para chegar ao mês anterior ao inputado
-# this while function gets us to the month before the input
+# while function to get to the month before the input
 def adjustDate():
+    # criação da variável aim, com a data fixada no dia 15 (ano e mês irrelevantes)
+    # variable aim, with date fixed in day 15 (year and month irrelevant)
+    aim = datetime.date(2021, 12, 15)
     global aimDate
     global userDate
     while aimDate.strftime("%m") == userDate.strftime("%m"):
@@ -97,12 +139,11 @@ def adjustDate():
     # loop to get to the last weekday of the first 15-days (fortnight) of the month before the input
     while aimDate.strftime("%d") > aim.strftime("%d"):
         aimDate = aimDate - day
-
-# verifica o input do usuário na variável dataExata, e utiliza a função adjustDate se necessário
-# verifies user input in dataExata variable and uses function adjustDate, if needed.
-
-if dataExata == "2":
+# verifica o input do usuário na variável userOption, e utiliza a função adjustDate se necessário
+# verifies user input in userOption variable and uses function adjustDate, if needed.
+if userOption == "2":
     adjustDate()
+
 # função com loop para chegar em um dia útil, se necessário
 # loop function to get to a weekday, if needed
 def conferirData():
@@ -110,21 +151,21 @@ def conferirData():
     isValid = False
     while not isValid:
         if aimDate.strftime("%A") in weekend:
-            aimDate = aimDate - day
+            if userOption in ("1","2"):
+                aimDate = aimDate - day
+            if userOption == "3":
+                aimDate = aimDate + day
         else:
             isValid = True
-
-
 conferirData()
+
 # função site com todos passos após abertura do bcb
 # site function with all steps after site opening
 def site():
-    # apaga dados no campo
-    # clears field data
-    element.clear()
+    element.clear() # apaga dados no campo; clears field data
     # input da data no campo representado pela variável element
     # date input in the field represented by the element variable
-    element.send_keys(aimDate.strftime("%m/%d/%Y")
+    element.send_keys(aimDate.strftime("%m/%d/%Y"))
     # encontra as checkboxes e as pressiona se não estiverem pressionadas
     # find the checkboxes and presses them, if not already pressed
     select1 = driver.find_element(By.XPATH, "//tr[1]/td[1]/input")
@@ -133,14 +174,11 @@ def site():
     select2 = driver.find_element(By.XPATH, "//tr[2]/td[1]/input")
     if select2.is_selected() == False:
         select2.click()
-    # pressione executar
-    # press execute
-    select3 = driver.find_element(By.XPATH, "//div[9]/div/div/button").click()
+    select3 = driver.find_element(By.XPATH, "//div[9]/div/div/button").click() # pressione executar; press execute
     time.sleep(2)
 
-
-# loop para evitar erros. Se o programa não retornar valores, diminui um dia da data até encontrar resultados ou encerra o código se chegar a 7 repetições
-# loop to avoid errors. If the program doesn't return values it decreases day by day until finding a result or shuts down the code if it gets to 7 repetitions
+# loop para evitar erros. Se o programa não retornar valores, diminui ou aumenta um dia da data até encontrar resultados ou encerra o código se chegar a 7 repetições
+# loop to avoid errors. If the program doesn't return values it decreases or raises day by day until finding a result or shuts down the code if it gets to 7 repetitions
 def tryanderror():
     contador = 0
     global dolarCompra
@@ -163,28 +201,23 @@ def tryanderror():
             dolarCompra = dolarCompra.get_attribute("innerHTML")
             dolarVenda = dolarVenda.get_attribute("innerHTML")
         except:
-            aimDate = aimDate - day
-            conferirData()
-            contador += 1
+            if userOption in ("1", "2"):
+                aimDate = aimDate - day
+                conferirData()
+                contador += 1
+            if userOption == "3":
+                aimDate = aimDate + day
+                conferirData()
+                contador += 1
             if contador >= 7:
                 print(
                     "Não foi possível encontrar uma data válida para o período especificado."
                 )
-                driver.close()
-                print(
-                    "Lista das datas e respectivos preços de compra do dólar consultados:\n"
-                    + str(dataCompra)
-                )
-                print(
-                    "\nLista das datas e respectivos preços de venda do dólar consultados: \n"
-                    + str(dataVenda)
-                )
-                sys.exit("\nFim do programa")
-
-
+                endProgram()
 tryanderror()
-# output do programa
-# program output
+
+# função de output do programa
+# program output function
 def output():
     global aimDate
     global dolarCompra
@@ -199,14 +232,23 @@ def output():
     dataVenda[aimDate.strftime("%d/%m/%Y")] = dolarVenda
     print("\nO preço de compra do dólar no dia era de: R$" + str(dolarCompra))
     print("\nO preço de venda do dólar no dia era de: R$" + str(dolarVenda) + "\n")
-
-
 output()
+
+# para a busca por período, faz um loop enquanto a primeira data for menor do que a data final
+# in search by time period, loops whilst first date is earlier than final date
+if userOption == "3":
+    while aimDate < finalDate:
+        aimDate = aimDate + day
+        conferirData()
+        dolarCompra = "0"
+        tryanderror()
+        output()
+
 # repetição para inserir mais datas
 # loop to get more dates
 while userDate != "0":
     inputDate()
-    if dataExata == "2":
+    if userOption == "2":
         adjustDate()
     conferirData()
     dolarCompra = "0"
